953bae6cf4ddb22fc53c2da401c72a1410a42b3a
Nima Goniband <n.goniband@gmail.com>
2016-05-14 13:27:28 +0200

    Structure Complete

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,32 @@
+import java.util.Scanner;
 
 public class GUI {
+
+	
+	private static Logic game;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//initialize the game
+		init();
+		
+	    
+	    
+	    System.out.println(game.getNrOfPlayers());
+
+	}
+	
+	public static void init(){
+		//ask how many players
+	    int players;
+	    Scanner in = new Scanner(System.in);
+	    
+	    System.out.println("How many players?: ");
+	    players = in.nextInt();
+	    
+	    //create the objekt with the right amount of players
+	    game = new Logic(players);
+	}
 
 }
