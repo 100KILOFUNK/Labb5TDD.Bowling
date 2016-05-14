61bbb727d11427a113268c82d1f0f390a215a983
Nima Goniband <n.goniband@gmail.com>
2016-05-14 17:16:58 +0200

    req two is implemented

--- src/Lab5BowlingTest.java
+++ src/Lab5BowlingTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class Lab5BowlingTest {
 	
-	GUI testGui;
+	
 
 	@Before
 	public void setUp() throws Exception {
@@ -14,24 +14,44 @@ public class Lab5BowlingTest {
 
 	@Test
 	public void test() {
-		//frametest
-		frametest();
+		//frametest - req 1
+		//reqone();
 		
+		//Sumtest - req 2
+		reqtwo();
 		
 	}
 	
-	public void frametest(){
-		Frame fr;
-		Logic lg;
+	public void reqone(){ //frametest
+		Frame fr; //create the frame which holds the values
+		Logic lg; //create the class which handles the logic, i e the throws
 		
 		lg = new Logic();
-		int first = lg.turn();
-		int second = lg.turn();
+		int first = lg.turn(); //throw the ball
+		int second = lg.turn();//throw the second ball
 		
-		fr = new Frame(first, second);
+		fr = new Frame(first, second); //create new frame
 		
-		assertTrue(fr.getFirst() <= 10 && fr.getFirst() >= 0);
-		assertTrue(fr.getSecond() <= 10 - fr.getFirst() && fr.getSecond() >= 0);
+		assertTrue(fr.getFirst() <= 10 && fr.getFirst() >= 0); //the range of the first throw 0-10. is it out of bounds?
+		assertTrue(fr.getSecond() <= 10 - fr.getFirst() && fr.getSecond() >= 0); //range of the second throw 0-(10-first). is it out of bounds?
 	}
+	
+	public void reqtwo(){
+		
+		Frame fr; //create the frame which holds the values
+		Logic lg; //create the class which handles the logic, i e the throws
+		
+		lg = new Logic();
+		int first = lg.turn(); //throw the ball
+		int second = lg.turn();//throw the second ball
+		
+		fr = new Frame(first, second); //create new frame
+		
+		int sum = first + second; //get the sum
+		assertEquals(sum, fr.returnSum()); //test if the two sums match
+		
+	}
+	
+	
 
 }
